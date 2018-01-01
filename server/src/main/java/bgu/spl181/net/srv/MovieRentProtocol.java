3da6dc2edbbc24f6aa9e6d69d6fc9899b9defa46
EdenAzulay <34909655+EdenAzulay@users.noreply.github.com>
2018-01-01 20:25:59 +0200

    Merge branch 'master' of https://github.com/EdenAzulay/Assignment3.1
    # Conflicts:
    #	server/src/main/java/bgu/spl181/net/api/bidi/Connection.java

--- server/src/main/java/bgu/spl181/net/srv/MovieRentProtocol.java
+++ server/src/main/java/bgu/spl181/net/srv/MovieRentProtocol.java
@@ -1,13 +1,30 @@
 package bgu.spl181.net.srv;
-
 import bgu.spl181.net.api.bidi.BidiMessagingProtocol;
 import bgu.spl181.net.api.bidi.Connections;
+import bgu.spl181.net.impl.rci.*;
+import bgu.spl181.net.srv.*;
 
-public class MovieRentProtocol<String> implements BidiMessagingProtocol<String> {
+public class MovieRentProtocol implements BidiMessagingProtocol {
     private boolean shouldTerminate=false;
-    public void start(int connectionId, Connections<String> connections){}
+    public void start(int connectionId, Connections<String> connections){
+        boolean bol;
+    }
 
-    public void process(String message){}
+    public void process(String msg){
+       String StrCommand=msg.substring(0,msg.indexOf('<'));
+       String args=msg.substring(msg.indexOf('<'),msg.length());
+       Command<String> cm;
+        switch (StrCommand){
+            case "ACK": cm= new ACKCommand(args);
+            case "ERROR":cm= new ERRORCommand(args);
+            case "BROADCAST":cm=new BROADCASTCommand(args);
+            case "REGISTER":cm=new REGISTERCommand(args);
+            case "LOGIN": cm=new LOGINCommand(args);
+            case "SIGNOUT":cm=new SIGNOUTCommand(args);
+            case "REQUEST":cm=new REQUESTCommand(args);
+        }
+        return cm.execute();
+    }
 
     /**
      * @return true if the connection should be terminated
