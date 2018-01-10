13356a6f499fc76bca1a5cd383721d8dcf59243a
snirshemtov <34773946+snirshemtov@users.noreply.github.com>
2018-01-10 17:04:47 +0200

    /

--- server/src/main/java/bgu/spl181/net/srv/TPCServer.java
+++ server/src/main/java/bgu/spl181/net/srv/TPCServer.java
@@ -2,7 +2,12 @@ package bgu.spl181.net.srv;
 
 import bgu.spl181.net.api.MessageEncoderDecoder;
 import bgu.spl181.net.api.bidi.BidiMessagingProtocol;
-import bgu.spl181.net.srv.bidi.ConnectionsImpl;
+import bgu.spl181.net.impl.BBreactor.ConnectionsImpl;
+import bgu.spl181.net.impl.BBtpc.BlockingConnectionHandler;
+//import bgu.spl181.net.impl.BBtpc.ConnectionsImpl;
+
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -10,17 +15,19 @@ import java.net.Socket;
 import java.util.function.Supplier;
 
 public class TPCServer<T> implements Server<T> {
-//blalalala
     private int port;
     private Supplier<BidiMessagingProtocol<T>> protocolFactory;
     private Supplier<MessageEncoderDecoder<T>> encoderDecoderFactory;
     private ServerSocket sock;
+
+//    private Gson gson;
 
     public TPCServer(int port, Supplier<BidiMessagingProtocol<T>> protocolFactory, Supplier<MessageEncoderDecoder<T>> encoderDecoderFactory) {
         this.port = port;
         this.protocolFactory = protocolFactory;
         this.encoderDecoderFactory = encoderDecoderFactory;
         this.sock = null;
+        //gson = new GsonBuilder().create();
     }
 
     @Override
@@ -30,7 +37,12 @@ public class TPCServer<T> implements Server<T> {
             this.sock = serverSock;
             System.out.println("Server started");
 
+            //create ConnectionsImpl
             ConnectionsImpl<T> connections = new ConnectionsImpl<>();
+
+            //create DBs
+            //private static Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+
 
             while (!Thread.currentThread().isInterrupted()) {
                 int availableUserId = connections.generateConnectionId();
